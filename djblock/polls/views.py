dd9892eaf8de8e557d178e62da6f7d64467a3e21
kantanand <kantanand.usk@gmail.com>
2018-04-19 20:23:18 +0530

    polls data brought from index function

--- djblock/polls/views.py
+++ djblock/polls/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = "no questions"
+    if latest_question_list:
+        output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
